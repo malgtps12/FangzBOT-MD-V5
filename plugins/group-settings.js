a9011001d996644205acf277f911cb6859dcc740
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-08-10 15:24:48 +0700

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -5,7 +5,7 @@ let text = `*${htki} GROUP ${htka}*
               📮ᴍᴀᴜ ᴍᴏᴅᴇ ᴀᴘᴀ?
 `
 const templateButtons = [
-    {index: 3, urlButton: {displayText: '💬 ᴏᴡɴᴇʀ', url: 'https://wa.me/6288215689772'}},
+    {index: 3, urlButton: {displayText: '💬 ᴏᴡɴᴇʀ', url: 'https://wa.me/6289516353968'}},
     {index: 4, quickReplyButton: {displayText: 'Open', id: '.group open'}},
     {index: 5, quickReplyButton: {displayText: 'Close', id: '.group close'}},
 ]
